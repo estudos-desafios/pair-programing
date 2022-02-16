d6ceeae651f773ab5bfdace5af0b2533c28d198c
Léo Moraes <moraes.leo@gmail.com>
2022-02-16 18:40:50 -0300

    Create CategoriaController

---
+++ app/Http/Controllers/CategoriaController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Categoria;
+
+class CategoriaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $categorias = Categoria::all();
+
+        return $categorias;
+
+    }
+
+    /**
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function store()
+    {
+        $categoria = Categoria::create([
+            'nome' => 'Categoria Teste',
+        ]);
+
+        $categoria->save();
+
+        return $categoria;
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $user = Categoria::find($id);
+
+        return $user;
+    }
+
+    /** Update the speficied resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $updateUser = Categoria::find($id);
+        $updateUser->nome = $request->nome;
+        $updateUser->save();
+
+        return $updateUser;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $deleteUser = Categoria::find($id);
+
+        $deleteUser->delete();
+
+        return $deleteUser;
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -12,8 +12,15 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+
 });
+
+/** Create Route User */
+// Route::resource(name:'users', controller:\App\Http\Controllers\UserController::class);
+
+/** Create Route Categorias */
+Route::resource(name:'categorias', controller:\App\Http\Controllers\CategoriaController::class);
